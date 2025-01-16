5421289d8ca06742b0e479a4f9dae085b78e53e0
Allan Araújo lima <allanvoide@gmail.com>
2025-01-15 22:36:49 -0300

    feat: using aws to keep the suggestions images

--- src/modules/sugestions/sugestions.controller.ts
+++ src/modules/sugestions/sugestions.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Body, Put, Param, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Patch, UseInterceptors, Request, UploadedFile } from '@nestjs/common';
 import { SugestionsService } from './sugestions.service';
+import { CreateSugestion } from './dto/create-sugestion.dto';
 import { UpdateSugestion } from './dto/update-sugestion.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('sugestions')
 export class SugestionsController {
@@ -17,10 +19,19 @@ export class SugestionsController {
     }
 
     @Post()
-    create(@Body() createSugestionDto: any) {
-        return this.sugestionsService.create(createSugestionDto);
-    }
+    @UseInterceptors(FileInterceptor('file'))
+    async create(
+        @Body() createSugestionDto: CreateSugestion,
+        @UploadedFile() file: Express.Multer.File
+    ) {
+        const sugestion = await this.sugestionsService.create(createSugestionDto);
 
+        const sugestionId = sugestion['id'];
+
+        this.sugestionsService.uploadFile(file, sugestionId);
+
+        return sugestion;
+    }
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateSugestion: UpdateSugestion) {
         return this.sugestionsService.update(id, updateSugestion)
--- src/modules/sugestions/sugestions.service.ts
+++ src/modules/sugestions/sugestions.service.ts
@@ -3,13 +3,24 @@ import { Repository } from 'typeorm';
 import { Sugestions } from './repository/sugestions.entity';
 import { UpdateSugestion } from './dto/update-sugestion.dto';
 import { Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class SugestionsService {
+
+    private AWS_S3_BUCKET = "wedding-now";
+
+    private s3: AWS.S3;
+
     constructor(
         @InjectRepository(Sugestions)
         private readonly sugestionsRepository: Repository<Sugestions>,
-    ) { }
+    ) {
+        this.s3 = new AWS.S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        });
+    }
 
     findAll() {
         return this.sugestionsRepository.find();
@@ -26,5 +37,35 @@ export class SugestionsService {
 
     update(id: string, updateSugestion: UpdateSugestion) {
         return this.sugestionsRepository.update(id, updateSugestion)
+    }
+
+    async uploadFile(file, sugestionId) {
+        return await this.s3_upload(
+            file.buffer,
+            this.AWS_S3_BUCKET,
+            sugestionId,
+            file.mimetype,
+        );
+    }
+
+    async s3_upload(file, bucket, name, mimetype) {
+        const params = {
+            Bucket: bucket,
+            Key: String(name),
+            Body: file,
+            ACL: 'public-read',
+            ContentType: mimetype,
+            ContentDisposition: 'inline',
+            CreateBucketConfiguration: {
+                LocationConstraint: 'ap-south-1',
+            },
+        };
+
+        try {
+            let s3Response = await this.s3.upload(params).promise();
+            return s3Response;
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
